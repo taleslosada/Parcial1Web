6f869b40be55b2f7dd0ba143dc3be3ce28f8eb82
Tales Losada <78095146+taleslosada@users.noreply.github.com>
2023-10-01 19:42:53 -0500

    finishing

--- src/login.js
+++ src/login.js
@@ -1,77 +1,143 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import loginData from "./login.json";
+import { FormattedMessage, useIntl } from "react-intl";
 
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [step, setStep] = useState(1); // 1 para el paso de correo, 2 para el paso de contraseña
-  const [emailError, setEmailError] = useState('');
-  const [passwordError, setPasswordError] = useState('');
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const [emailValid, setEmailValid] = useState(true);
+  const [passwordValid, setPasswordValid] = useState(true);
+  const [error, setError] = useState("");
+  const [step, setStep] = useState(1); 
 
-  const validateEmail = () => {
-    // Validación de formato de correo electrónico 
-    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    if (!email.match(emailRegex)) {
-      setEmailError('El correo electrónico no es válido');
-      return false;
-    }
-    setEmailError('');
-    return true;
+  const intl = useIntl(); 
+
+  const handleEmailChange = (e) => {
+    setFormData({ ...formData, email: e.target.value });
+    validateEmail(e.target.value);
   };
 
-  const validatePassword = () => {
-    // Validación de longitud mínima de la contraseña
-    if (password.length < 6) {
-      setPasswordError('La contraseña debe tener al menos 6 caracteres');
-      return false;
-    }
-    setPasswordError('');
-    return true;
+  const handlePasswordChange = (e) => {
+    setFormData({ ...formData, password: e.target.value });
+    validatePassword(e.target.value);
   };
 
-  const handleNextStep = () => {
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isEmailValid = emailRegex.test(email);
+    setEmailValid(isEmailValid);
+  };
+
+  const validatePassword = (password) => {
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/;
+    const isPasswordValid = passwordRegex.test(password);
+    setPasswordValid(isPasswordValid);
+  };
+
+  const changeStep = () => {
     if (step === 1) {
-      if (validateEmail()) {
+      if (emailValid) {
+        setError("");
         setStep(2);
+      } else {
+        setError(intl.formatMessage({ id: "login.emailInvalid" })); 
       }
     } else if (step === 2) {
-      if (validatePassword()) {
-        // Realizar la autenticación aquí (por ejemplo, enviar una solicitud POST al backend)
-        // Si la autenticación es exitosa, puedes redirigir al usuario a la página principal
+      if (passwordValid) {
+        addUserToJSON();
+      } else {
+        setError(intl.formatMessage({ id: "login.passwordInvalid" })); 
       }
     }
+  };
+
+  const generateRandomRole = () => {
+    return Math.random() < 0.5; 
+  };
+
+  const addUserToJSON = () => {
+    if (!emailValid || !passwordValid) {
+      setError(intl.formatMessage({ id: "login.invalidCredentials" })); 
+      return;
+    }
+
+    console.log("Agregando", formData);
+    const newUser = {
+      id: loginData.length + 1, 
+      email: formData.email,
+      password: formData.password,
+      role: generateRandomRole(), 
+    };
+
+    loginData.push(newUser);
+
+    navigate("/Home", { state: { userRole: newUser.role } });
   };
 
   return (
-    <div>
-      {step === 1 ? (
-        <div>
-          <h1>Ingrese su correo electrónico</h1>
-          <input
-            type="email"
-            placeholder="Correo electrónico"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          {emailError && <p className="error">{emailError}</p>}
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-12">
+          {error && <div className="alert alert-danger">{error}</div>}
+          {step === 1 && (
+            <form>
+              <div className="form-group">
+                <h2>
+                  <FormattedMessage id="login.loginHeader" />
+                </h2>
+                <h4>
+                  <FormattedMessage id="login.useUniAlpesAccount" />
+                </h4>
+                <label>
+                  <FormattedMessage id="login.emailLabel" />
+                </label>
+                <input
+                  type="email"
+                  className={`form-control ${emailValid ? "" : "is-invalid"}`}
+                  value={formData.email}
+                  onChange={handleEmailChange}
+                />
+                {!emailValid && (
+                  <div className="invalid-feedback">
+                    <FormattedMessage id="login.emailInvalidMessage" />
+                  </div>
+                )}
+              </div>
+              <button type="button" className="btn btn-primary" onClick={changeStep}>
+                <FormattedMessage id="login.nextButton" />
+              </button>
+            </form>
+          )}
+          {step === 2 && (
+            <form>
+              <div className="form-group">
+                <h4>{formData.email}</h4>
+                <label>
+                  <FormattedMessage id="login.passwordLabel" />
+                </label>
+                <input
+                  type="password"
+                  className={`form-control ${passwordValid ? "" : "is-invalid"}`}
+                  value={formData.password}
+                  onChange={handlePasswordChange}
+                />
+                {!passwordValid && (
+                  <div className="invalid-feedback">
+                    <FormattedMessage id="login.passwordInvalidMessage" />
+                  </div>
+                )}
+              </div>
+              <button type="button" className="btn btn-primary" onClick={changeStep}>
+                <FormattedMessage id="login.loginButton" />
+              </button>
+            </form>
+          )}
         </div>
-      ) : (
-        <div>
-          <h1>Ingrese su contraseña</h1>
-          <p>Correo ingresado: {email}</p>
-          <input
-            type="password"
-            placeholder="Contraseña"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          {passwordError && <p className="error">{passwordError}</p>}
-        </div>
-      )}
-      <button onClick={handleNextStep}>Siguiente</button>
+      </div>
     </div>
   );
 }
 
-
 export default Login;
-
