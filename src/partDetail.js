16a2cd0043f6875895739481a24c1c471b372aac
Tales Losada <78095146+taleslosada@users.noreply.github.com>
2023-10-01 19:37:44 -0500

    finishing

--- src/partDetail.js
+++ src/partDetail.js
@@ -1,28 +1,125 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
+import React, { useState } from "react";
+import { Card, Button, Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { FormattedMessage, useIntl } from "react-intl"; 
+import "./partDetail.css";
 
 function PartDetail() {
-  const { id } = useParams();
-  const [part, setPart] = useState(null);
+  const location = useLocation();
+  const { state: { part, userRole } } = location;
+  const [editable, setEditable] = useState(userRole);
+  const intl = useIntl();
 
-  useEffect(() => {
-    // Realizar una solicitud GET al endpoint `/parts/${id}` para obtener los detalles de la parte específica
-    // Actualiza el estado "part" con la respuesta del servidor
-  }, [id]);
-
-  if (!part) {
-    return <div>Cargando...</div>;
-  }
+  const handleEditToggle = () => {
+    setEditable(!editable);
+  };
 
   return (
-    <div>
-      <h1>Detalles de la Parte</h1>
-      <img src={part.image} alt={part.name} />
-      <h2>{part.name}</h2>
-      <p>Marca: {part.brand}</p>
-      <p>Precio: ${part.price}</p>
-      <p>Año: {part.year}</p>
-      <p>Descripción: {part.description}</p>
+    <div className="part-detail">
+      <Container>
+        <h2>
+          <FormattedMessage id="partDetail.detailHeader" />
+        </h2>
+        <Row>
+          <Col md={7}>
+            <Card.Img variant="top" src={part.image} alt={part.carModel} />
+          </Col>
+          <Col md={5}>
+            <Card className="text-Detail">
+              <Card.Body>
+                <Card.Title>{part.partName}</Card.Title>
+                {editable ? (
+                  <div>
+                    {}
+                    <Card.Text>
+                      <FormattedMessage id="partDetail.madeBy" />
+                      <input
+                        type="text"
+                        value={part.carMaker}
+                        onChange={(e) => part.carMaker = e.target.value}
+                      />
+                    </Card.Text>
+                    <Card.Text>
+                      <FormattedMessage id="partDetail.model" />
+                      <input
+                        type="text"
+                        value={part.carModel}
+                        onChange={(e) => part.carModel = e.target.value}
+                      />
+                    </Card.Text>
+                    <Card.Text>
+                      <FormattedMessage id="partDetail.year" />
+                      <input
+                        type="text"
+                        value={part.carYear}
+                        onChange={(e) => part.carYear = e.target.value}
+                      />
+                    </Card.Text>
+                    <Card.Text>
+                      <FormattedMessage id="partDetail.available" />
+                      <input
+                        type="checkbox"
+                        checked={part.avalaible}
+                        onChange={(e) => part.avalaible = e.target.checked}
+                      />
+                    </Card.Text>
+                    <Card.Text>
+                      <FormattedMessage id="partDetail.description" />
+                      <input
+                        type="text"
+                        value={part.description}
+                        onChange={(e) => part.description = e.target.value}
+                      />
+                    </Card.Text>
+                    <Card.Text>
+                      <FormattedMessage id="partDetail.price" />
+                      <input
+                        type="text"
+                        value={part.price}
+                        onChange={(e) => part.price = e.target.value}
+                      />
+                    </Card.Text>
+                    <Button onClick={handleEditToggle}>
+                      <FormattedMessage id="partDetail.saveChanges" />
+                    </Button>
+                  </div>
+                ) : (
+                  <div>
+                    {}
+                    <Card.Text>
+                      <FormattedMessage id="partDetail.madeBy" />: <p>{part.carMaker}</p>
+                    </Card.Text>
+                    <Card.Text>
+                      <FormattedMessage id="partDetail.model" />: <p>{part.carModel}</p>
+                    </Card.Text>
+                    <Card.Text>
+                      <FormattedMessage id="partDetail.year" />: <p>{part.carYear}</p>
+                    </Card.Text>
+                    <Card.Text>
+                      <FormattedMessage id="partDetail.description" />: <p>{part.description}</p>
+                    </Card.Text>
+                    <Card.Text>
+                    <FormattedMessage id="partDetail.price" />:{" "}
+                    {intl.formatNumber(
+                      parseFloat(part.price.replace("$", "")) *
+                        (intl.locale === "es-ES" || intl.locale === "es" ? 3800 : 1), 
+                      {
+                        style: "currency",
+                        currency: intl.locale === "es" ? "COP" : "USD",
+                      }
+                    )}
+                  </Card.Text>
+                  </div>
+                )}
+                <Link to="/Home" state={{ userRole: userRole }}>
+                  <FormattedMessage id="partDetail.backToList" />
+                </Link>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
