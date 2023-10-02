8373e0a6c4c95909323b2c03eee038fe33370465
Tales Losada <78095146+taleslosada@users.noreply.github.com>
2023-10-01 19:42:03 -0500

    finishing

--- src/home.js
+++ src/home.js
@@ -1,28 +1,75 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import data from "./datos.json";
+import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import { FormattedMessage } from "react-intl"; 
 
 function Home() {
-  const [parts, setParts] = useState([]);
+  const [cars, setCars] = useState(data);
+  const [selectedcar, setSelectedcar] = useState(null);
+  const [error, setError] = useState("");
+  const { state: { userRole } } = useLocation();
+
+  const handlecarClick = (carModel) => {
+    const car = cars.find((car) => car.carModel === carModel);
+    setSelectedcar(car);
+  };
 
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://github.com/taleslosada/Parcial1Web/blob/main/src/datos.json");
+        if (!response.ok) {
+          throw new Error("Error");
+        }
+        const data = await response.json();
+        setCars(data);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+    fetchData();
   }, []);
 
   return (
-    <div>
-      <h1>Escudería Orakle - Inventario</h1>
-      <div className="parts-grid">
-        {parts.map((part) => (
-          <div className="part-card" key={part.id}>
-            <Link to={`/part/${part.id}`}>
-              <img src={part.image} alt={part.name} />
-              <h2>{part.name}</h2>
-              <p>Marca: {part.brand}</p>
-              <p>Precio: ${part.price}</p>
-              <p>Año: {part.year}</p>
-            </Link>
-          </div>
-        ))}
-      </div>
-    </div>
+    <Container className="home-container">
+      <Row>
+        <Col md={12}>
+          <h2>
+            <FormattedMessage id="home.catalogHeader" /> {}
+          </h2>
+          <Row>
+            {cars.map((car) => (
+              <Col md={3} key={car.carModel}>
+                <Card
+                  className={`car-card ${
+                    selectedcar && selectedcar.carModel === car.carModel ? "selected" : ""
+                  }`}
+                >
+                  <Card.Img variant="top" src={car.image} alt={car.carModel} />
+                  <Card.Body>
+                    <Card.Title>{car.partName}</Card.Title>
+                    <Link
+                      to={`/car/${car.carModel}`}
+                      state={{ car, userRole }}
+                      className="car-link"
+                    >
+                      <Button
+                        variant="primary"
+                        onClick={() => handlecarClick(car.carModel)}
+                      >
+                        <FormattedMessage id="home.viewDetailsButton" /> 
+                      </Button>
+                    </Link>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
